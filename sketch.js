629283f44086b7d6baba23e5095c296ddad4d96f
pro-whitehatjr <80043809+pro-whitehatjr@users.noreply.github.com>
2021-05-11 21:25:06 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+let ground;
+let lander;
+var lander_img;
+var bg_img;
+
+
+var vx = 0;
+var g = 0.05;
+var vy = 0;
+
+function preload()
+{
+  lander_img = loadImage("normal.png");
+  bg_img = loadImage("bg.png");
+}
+
+function setup() {
+  createCanvas(1000,700);
+  frameRate(80);
+
+  lander = createSprite(100,50,30,30);
+  lander.addImage(lander_img);
+  lander.scale = 0.1;
+
+  rectMode(CENTER);
+  textSize(15);
+}
+
+function draw() 
+{
+  background(51);
+  image(bg_img,0,0);
+  push()
+  fill(255);
+  text("Vertical Velocity: "+round(vy),800,75);
+  pop();
+
+  //fall down
+  vy +=g;
+  lander.position.y+=vy;
+  drawSprites();
+}
+
+
